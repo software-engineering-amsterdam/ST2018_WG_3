d730a16b84f2d5a94a79945f0324b679b8da42aa
Javier Bermejo <javieritoelbelga@hotmail.com>
2018-09-10 18:37:54 +0200

    done all exercises with some parts of it missing

--- javier/Lab1/Lab1.hs
+++ javier/Lab1/Lab1.hs
@@ -2,6 +2,7 @@
 module Lab1 where
 import Data.List
 import Test.QuickCheck
+import System.Random
 
 prime :: Integer -> Bool
 prime n = n > 1 && all (\ x -> rem n x /= 0) xs
@@ -110,15 +111,119 @@ myPermTest xs = ((length xs) < 10) --> lengthPerms xs == factorialLength xs
 -- We are testing if our perms implementation work. We are using mathematical
 -- facts in order to know if this implementation is correct
 
--- Exercise 4 (30 min)
+-- Exercise 4 (1:30H)
 primeReversal :: Integer -> [Integer]
 primeReversal n = [x | x <- [1..n], let b = reversal x, prime x && prime b]
 
-notPrime :: Int -> Bool
-notPrime = not prime
+notPrime :: Integer -> Bool
+notPrime x = not (prime x)
 
-primeReversalTest (x:xs) =
-  if notPrime x
+--primeReversalTest (x:xs) =
+--if notPrime x
+--dont know how to do it
 
--- Exercise 5
-sum101Primes = 
+-- Exercise 5 (40 Mins)
+sum101Primes :: [Integer] -> Integer
+sum101Primes xs
+  | prime (sum(take 101 xs)) = sum(take 101 xs)
+  | otherwise = sum101Primes (drop 1 xs)
+
+doSum101Primes :: Integer
+doSum101Primes = sum101Primes primes
+
+-- We are not going to test this function, as we use library Functions
+-- (that we asume that work), and functions that have been already tested
+-- (like for example, prime or primes). We are taking a list of primes, and
+-- summing the 101 first results. If this doesn't give a prime number, then
+-- we remove the first in the list, and we sum the next 101 numbers. We repeat
+-- this till we find the prime number
+
+-- Exercise 6 (30 Mins)
+calculateConjeture :: Int -> Bool
+calculateConjeture n = prime ((product(take n primes)) + 1)
+
+counterExamples :: Int -> [Integer]
+counterExamples n
+  | calculateConjeture n = counterExamples (n+1)
+  | otherwise = take n primes
+
+doCounterExamples :: [Integer]
+doCounterExamples = counterExamples 2
+
+-- Exercise 7 (2h without testing)
+intToDigs :: Int->[Int]
+intToDigs 0 = []
+intToDigs x = intToDigs (x `div` 10) ++ [x `mod` 10]
+
+--Taken from here https://stackoverflow.com/questions/19867491/double-every-other-element-of-list-from-right-in-haskell
+--I had the same idea, only i didn't know the syntax
+doublePairs :: [Int] -> [Int]
+doublePairs xs = fst (foldr (\x (numbers,bool) ->
+                              ((if bool then 2 * x else x) : numbers, not bool))
+                              ([], False) xs)
+
+sumBigNumbers :: [Int] -> [Int]
+sumBigNumbers xs = [sum (intToDigs x) | x <- xs]
+
+sumNumbers :: [Int] -> Int
+sumNumbers xs = sum xs
+
+luhn :: Int -> Bool
+luhn x = sumNumbers(sumBigNumbers(doublePairs (intToDigs x))) `mod` 10 == 0
+
+checkVisa :: Int -> Bool
+checkVisa x = x `div` 1000000000000000 == 4
+
+isVisa :: Int -> Bool
+isVisa x = (checkVisa x) && (luhn x)
+
+checkMaster :: Int -> Bool
+checkMaster x = (x `div` 100000000000000) `elem` [51..55]
+
+isMaster :: Int -> Bool
+isMaster x = (checkMaster x) && (luhn x)
+
+checkAmericanExpress :: Int -> Bool
+checkAmericanExpress x = (x `div` 100000000000000) `elem` [34,37]
+
+isAmericanExpress :: Int -> Bool
+isAmericanExpress x = (checkAmericanExpress x) && (luhn x)
+
+-- We only have to test luhn implementation, as the other functions are
+-- basic lisp implementations
+-- To test the luhn implementation, we are going to generate luhn numbers and
+-- see if it works
+
+--luhnGenerator :: Int
+--luhnGenerator = [100000000000000..999999999999999]
+
+
+-- Exercise 8 (2h)
+-- We define xor for the accuses function
+xor :: Bool -> Bool -> Bool
+xor True a = not a
+xor False a = a
+
+-- We define who accuses everyone, using the last function created
+accuses :: Boy -> Boy -> Bool
+accuses Matthew x = not (x == Matthew) && not (x == Carl)
+accuses Peter x = (x == Matthew) || (x == Jack)
+accuses Jack x = not (accuses Matthew x) && not (accuses Peter x)
+accuses Arnold x = (accuses Matthew x) `xor` (accuses Peter x)
+accuses Carl x = not (accuses Arnold x)
+
+-- We want to know the list of which person is accused by
+accusers :: Boy -> [Boy]
+accusers boy = filter (\x -> accuses x boy) boys
+
+-- We want to know who is guilty.
+-- We know that only 3 people are telling the truth, so if we verify that
+-- We will know who was guilty
+guilty :: [Boy]
+guilty = filter (\x -> length (accusers x) == 3) boys
+
+--We want to know who said the truth
+-- We know that 2 people are saying lies, and they dont accuse Jack
+-- We know that 3 people said the truth and accused Jack
+honest :: [Boy]
+honest = filter (\x -> accuses x Jack) boys
